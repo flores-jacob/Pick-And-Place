769ea6ff8a22452771b21b2eadb68f1f11e9e70c
phwebmachine <phwebmachine@gmail.com>
2017-07-11 23:17:28 +0800

    bug: Use correct variables px, py, pz, roll, pitch, yaw instead of theta

--- error_plotter.py
+++ error_plotter.py
@@ -6,23 +6,23 @@ from plotly.offline import plot
 # from IK import T_total
 
 # compute the FK, then compute the errors
-# FK_matrix = T_total.evalf(subs={q1: theta1, q2: theta2, q3: theta3, q4: theta4, q5: theta5, q6: theta6})
+# FK_matrix = T_total.evalf(subs={q1: px, q2: py, q3: pz, q4: roll, q5: pitch, q6: yaw})
 
 # how to jsonify numpy arrays https://stackoverflow.com/a/32850511
-theta1_list = np.random.uniform(low = -0.1, high=0.1, size=50).tolist()
-theta2_list = np.random.uniform(low = -0.1, high=0.1, size=50).tolist()
-theta3_list = np.random.uniform(low = -0.1, high=0.1, size=50).tolist()
-theta4_list = np.random.uniform(low = -0.1, high=0.1, size=50).tolist()
-theta5_list = np.random.uniform(low = -0.1, high=0.1, size=50).tolist()
-theta6_list = np.random.uniform(low = -0.1, high=0.1, size=50).tolist()
+px_error_list = np.random.uniform(low = -0.1, high=0.1, size=50).tolist()
+py_error_list = np.random.uniform(low = -0.1, high=0.1, size=50).tolist()
+pz_error_list = np.random.uniform(low = -0.1, high=0.1, size=50).tolist()
+roll_error_list = np.random.uniform(low = -0.1, high=0.1, size=50).tolist()
+pitch_error_list = np.random.uniform(low = -0.1, high=0.1, size=50).tolist()
+yaw_error_list = np.random.uniform(low = -0.1, high=0.1, size=50).tolist()
 
 error_data = {
-    "theta1": theta1_list,
-    "theta2": theta2_list,
-    "theta3": theta3_list,
-    "theta4": theta4_list,
-    "theta5": theta5_list,
-    "theta6": theta6_list,
+    "px": px_error_list,
+    "py": py_error_list,
+    "pz": pz_error_list,
+    "roll": roll_error_list,
+    "pitch": pitch_error_list,
+    "yaw": yaw_error_list,
 }
 
 # save to disk
@@ -34,51 +34,49 @@ with open(error_list_path, "w") as error_list_file:
 with open(error_list_path, "r") as error_list_file:
     json_error_list = json.loads(error_list_file.read())
 
-print ("json error list", json_error_list)
 
-
-theta1_list = json_error_list["theta1"]
-theta2_list = json_error_list["theta2"]
-theta3_list = json_error_list["theta3"]
-theta4_list = json_error_list["theta4"]
-theta5_list = json_error_list["theta5"]
-theta6_list = json_error_list["theta6"]
+px_error_list = json_error_list["px"]
+py_error_list = json_error_list["py"]
+pz_error_list = json_error_list["pz"]
+roll_error_list = json_error_list["roll"]
+pitch_error_list = json_error_list["pitch"]
+yaw_error_list = json_error_list["yaw"]
 
 
 # prepare the data for the box plot for plotly to plot
 # box plot taken here https://plot.ly/python/box-plots/
-theta1_errors = Box(x=1,
-    y=theta1_list,
-    name='theta1'
-)
-theta2_errors = Box(
+px_errors = Box(x=1,
+                y=px_error_list,
+                name='px'
+                )
+py_errors = Box(
     x=2,
-    y=theta2_list,
-    name='theta2'
+    y=py_error_list,
+    name='py'
 
 )
-theta3_errors = Box(
+pz_errors = Box(
     x=3,
-    y=theta3_list,
-    name='theta3'
+    y=pz_error_list,
+    name='pz'
 
 )
-theta4_errors = Box(
+roll_errors = Box(
     x=4,
-    y=theta4_list,
-    name='theta4'
+    y=roll_error_list,
+    name='roll'
 
 )
-theta5_errors = Box(
+pitch_errors = Box(
     x=5,
-    y=theta5_list,
-    name='theta5'
+    y=pitch_error_list,
+    name='pitch'
 
 )
-theta6_errors = Box(
+yaw_errors = Box(
     x=6,
-    y=theta6_list,
-    name='theta6'
+    y=yaw_error_list,
+    name='yaw'
 )
-data = [theta1_errors, theta2_errors, theta3_errors, theta4_errors, theta5_errors, theta6_errors]
+data = [px_errors, py_errors, pz_errors, roll_errors, pitch_errors, yaw_errors]
 plot({"data": data})
